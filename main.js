231be2b48cc32382e05d8303589e7ae82a731d6d
ladew222 <egweinbe@gmail.com>
2022-11-05 11:19:02 -0500

    code

---
+++ main.js
@@ -0,0 +1,59 @@
+
+
+function csvToArray(str, delimiter = ",") {
+
+  // slice from start of text to the first \n index
+  // use split to create an array from string by delimiter
+  const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+
+  // slice from \n index + 1 to the end of the text
+  // use split to create an array of each csv value row
+  const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+  // Map the rows
+  // split values from each row into an array
+  // use headers.reduce to create an object
+  // object properties derived from headers:values
+  // the object passed as an element of the array
+  const arr = rows.map(function (row) {
+    const values = row.split(delimiter);
+    const el = headers.reduce(function (object, header, index) {
+      object[header] = values[index];
+      return object;
+    }, {});
+    return el;
+  });
+
+  // return the array
+  return arr;
+}
+
+async function loadFileAndPrintToConsole(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+function load(){
+    loadFileAndPrintToConsole('main.js');
+}
+
+
+if(myForm){
+    myForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const input = csvFile.files[0];
+        const reader = new FileReader();
+      
+        reader.onload = function (e) {
+          const text = e.target.result;
+          const data = csvToArray(text);
+          document.write(JSON.stringify(data));
+        };
+        
+        reader.readAsText(input);
+      });
+  }
